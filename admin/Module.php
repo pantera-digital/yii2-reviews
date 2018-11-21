819e38ab3e07c678102967995fa97943eacc022e
Alex Kostin <web-mayak@mail.ru>
2018-11-21 21:34:59 +1000

    add getMenuItems to admin/Module

--- admin/Module.php
+++ admin/Module.php
@@ -7,4 +7,11 @@ class Module extends \yii\base\Module
     /* @var array Массив ролей которым доступна админка */
     public $permissions = ['@'];
     public $reviewAdminClasses = [];
+
+    public function getMenuItems()
+    {
+        return [
+        	['label' => 'Отзывы', 'url' => ['/reviews/review'], 'icon' => 'comments'],
+    	];
+    }
 }
